5984508e970622aa77e710172bb92eb074f42af2
Nicholas Ochs <nick.ochs@yahoo.com>
2020-08-25 18:26:53 -0400

    fixing cipher, not complete yet

--- caesar-cipher.js
+++ caesar-cipher.js
@@ -2,8 +2,22 @@
 
 //thoughts: make a for loop to find the index of each character in the string and then somehow replace it with another character
 
-function cipher(someString, caesarString) {
-    someString.charAt(0)
+function cipher(someString, offset) {
+    const alphabet = 'abcdefghijklmnopqrstuvwxyz'
+    let newString = ''
+    for(i = 0; i < someString.length; i++) {
+        const preLetter = someString.charAt(i);
+        const indexLetter = alphabet.indexOf(preLetter);
+        const offsetIndex = indexLetter + offset;
+        if(offsetIndex > alphabet.length) {
+            offsetIndex = offsetIndex%alphabet.length
+        }
+        newString += alphabet.charAt(offsetIndex)
+    }
+    console.log(newString)
+    
 }
 
-cipher('test')
+cipher('test', 5)
+
+//make a conditional to handle spaces and periods ... make a conditional to check if the character is within my alphabet... if it is not, then just write character. if it is, then proceed with normal for loop above.
